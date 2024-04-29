62a72c474c67686538ac686520fd73d9c707b562
35404saba <163739815+35404saba@users.noreply.github.com>
2024-04-29 13:53:23 +0500

    todo-list

---
+++ index.js
@@ -0,0 +1,23 @@
+#!usr/bin/env/node
+import inquirer from "inquirer";
+let todos = []; //shopper[]
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            message: "what would you lik to add in your Todos?",
+            type: "input",
+        },
+        {
+            name: "secondQuestion",
+            message: "would you like  to add more in yout Todos?",
+            type: "confirm",
+            default: "true",
+        },
+    ]);
+    todos.push(todoQuestion.firstQuestion);
+    console.log(todos);
+    //the loop is running on the based of this variable condition
+    condition = todoQuestion.secondQuestion;
+}
